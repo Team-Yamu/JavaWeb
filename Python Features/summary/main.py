12b1154145747c19d36a08514bf50b622785744e
seungupnoh <soungho70_@naver.com>
2020-06-01 13:26:18 +0900

    up 동적으로 json 파일을 생성할 수 있도록 인자를 추가했습니다.
    k, s, p, a, t, m 인자를 추가했습니다.
    자세한 사항은 README.md 파일을 참고해주세요

--- Python Features/summary/main.py
+++ Python Features/summary/main.py
@@ -8,9 +8,34 @@ if __name__ == '__main__':
     parse.add_argument("-print", help="print to json", required=False, default=True)
     parse.add_argument("-post", help="post to url", required=False, default=False)
 
+    parse.add_argument('-k', help="Set Keyword", action='store_true')
+    parse.add_argument('-s', help="Set Summary", action='store_true')
+    parse.add_argument('-p', help="Set Publish Date", action='store_true')
+    parse.add_argument('-a', help="Set Article Text", action='store_true')
+    parse.add_argument('-t', help="Set Top Image Link", action='store_true')
+    parse.add_argument('-m', help="Set Movies Link", action='store_true')
+
     args = parse.parse_args()
 
     news = CNewsPaper3k(args.target)
+
+    if args.k:
+        news.setKeyword()
+
+    if args.s:
+        news.setSummary()
+
+    if args.p:
+        news.setPublishDate()
+
+    if args.a:
+        news.setArticleText()
+
+    if args.t:
+        news.setTopImage()
+
+    if args.m:
+        news.setMovies()
 
     if args.save:
         news.save(args.save)
